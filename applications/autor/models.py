c5b69c6b15aa8f8b0ecb1962a5034f397e884c69
Maizares, Juan <juan_maizares386@hotmail.com>
2022-03-24 12:40:00 -0300

    reestructura de modelo autor - persona

--- applications/autor/models.py
+++ applications/autor/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from .managers import AutorManager
 # Create your models here.
-
-class Autor(models.Model):
-    #CAMPOS
+class Persona (models.Model):
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     nacionalidad = models.CharField(max_length=30)
     edad = models.PositiveIntegerField()
+    def __str__(self):
+        return self.nombre + '-' + str(self.id)
+    class Meta:
+        abstract = True
+class Autor(Persona):
+    #CAMPOS
+    
     #MANAGER
     objects = AutorManager()
     #METODOS
-    def __str__(self):
-        return self.nombre + '-' + str(self.id)
+    
