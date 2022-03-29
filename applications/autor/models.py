658cfb3a56d49ca2e6f6c2ec92044075e33839c5
Maizares, Juan <juan_maizares386@hotmail.com>
2022-03-28 21:50:25 -0300

    reestructura de modelos - fix plural nombre

--- applications/autor/models.py
+++ applications/autor/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from .managers import AutorManager
 # Create your models here.
 class Persona (models.Model):
-    nombre = models.CharField(max_length=50)
+    nombres = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     nacionalidad = models.CharField(max_length=30)
     edad = models.PositiveIntegerField()
     def __str__(self):
-        return self.nombre + '-' + str(self.id)
+        return self.nombres + '-' + str(self.id)
     class Meta:
         abstract = True
 class Autor(Persona):
--- applications/lector/models.py
+++ applications/lector/models.py
@@ -1,16 +1,13 @@
+from tabnanny import verbose
 from django.db import models
 from applications.libro.models import Libro
+from applications.autor.models import Persona
 from .managers import PrestamoManager
 
 # Create your models here.
-class Lector(models.Model):
-    nombres = models.CharField(max_length=50)
-    apellidos = models.CharField(max_length=50)
-    nacionalidad = models.CharField(max_length=20)
-    edad = models.PositiveIntegerField(default=0)
-    
-    def __str__(self):
-        return self.nombres
+class Lector(Persona):
+    class Meta:
+        verbose_name='Lector'
     
 class Prestamo(models.Model):
     lector = models.ForeignKey(Lector, on_delete=models.CASCADE)
